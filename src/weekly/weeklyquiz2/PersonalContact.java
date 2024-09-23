498ed25f4416d29b6d9489920a8754294cea8127
icode1211 <icode1211@naver.com>
2024-09-23 17:11:17 +0900

    Java 자료구조 위클리 퀴즈(week3) 코드

---
+++ src/weekly/weeklyquiz2/PersonalContact.java
@@ -0,0 +1,15 @@
+package weekly.weeklyquiz2;
+
+public class PersonalContact extends Contact {
+    private String relationship;
+
+    public PersonalContact(String name, String phoneNumber, String relationship) {
+        super(name, phoneNumber);
+        this.relationship = relationship;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "관계: " + relationship;
+    }
+}
